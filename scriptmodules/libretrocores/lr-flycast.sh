701698d51502242cb0e7c7b4678364f70525951b
Mike P <63381908+mictjs@users.noreply.github.com>
2020-04-24 20:11:48 -0300

    Updates lr-flycast

--- scriptmodules/libretrocores/lr-flycast.sh
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -10,8 +10,8 @@
 #
 
 rp_module_id="lr-flycast"
-rp_module_desc="Dreamcast emulator - Reicast port for libretro"
-rp_module_help="Previously named lr-reicast then lr-beetle-dc\n\nDreamcast ROM Extensions: .cdi .gdi .chd, Naomi/Atomiswave ROM Extension: .zip\n\nCopy your Dreamcast/Naomi roms to $romdir/dreamcast\n\nCopy the required Dreamcast BIOS files dc_boot.bin and dc_flash.bin to $biosdir/dc\n\nCopy the required Naomi/Atomiswave BIOS files naomi.zip and awbios.zip to $biosdir/dc"
+rp_module_desc="Dreamcast/NAOMI/Atomiswave emulator - Reicast port for libretro"
+rp_module_help="Dreamcast ROM Extensions: .cdi .gdi .chd\nNaomi/Atomiswave ROM Extension: .zip\n\nCopy your Dreamcast roms to $romdir/dreamcast\nCopy your NAOMI roms to $romdir/naomi\nCopy your Atomiswave roms to $romdir/atomiswave\n\nCopy the required BIOS files dc_boot.bin, dc_flash.bin, naomi.zip and awbios.zip to $biosdir/dc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/flycast/master/LICENSE"
 rp_module_section="opt"
 rp_module_flags="!mali !armv6"
@@ -47,7 +47,7 @@ function build_lr-flycast() {
         fi
     fi
     # temporarily disable distcc due to segfaults with cross compiler and lto
-    DISTCC_HOSTS="" make "${params[@]}"
+    DISTCC_HOSTS="" make "${params[@]}" -j`nproc`
     md_ret_require="$md_build/flycast_libretro.so"
 }
 
@@ -59,20 +59,23 @@ function install_lr-flycast() {
 }
 
 function configure_lr-flycast() {
-    mkRomDir "dreamcast"
-    ensureSystemretroconfig "dreamcast"
-
     mkUserDir "$biosdir/dc"
 
-    # system-specific
-    if isPlatform "gl"; then
-        iniConfig " = " "" "$configdir/dreamcast/retroarch.cfg"
-        iniSet "video_shared_context" "true"
-    fi
+    local system
+    for system in dreamcast naomi atomiswave; do
+        mkRomDir "$system"
+        ensureSystemretroconfig "$system"
 
-    local def=0
-    isPlatform "kms" && def=1
-    # segfaults on the rpi without redirecting stdin from </dev/null
-    addEmulator $def "$md_id" "dreamcast" "$md_inst/flycast_libretro.so </dev/null"
-    addSystem "dreamcast"
+	# system-specific
+	if isPlatform "gl"; then
+            iniConfig " = " "" "$configdir/$system/retroarch.cfg"
+            iniSet "video_shared_context" "true"
+	fi
+
+	local def=0
+	isPlatform "kms" && def=1
+	# segfaults on the rpi without redirecting stdin from </dev/null
+        addEmulator $def "$md_id" "$system" "$md_inst/flycast_libretro.so </dev/null"
+        addSystem "$system"
+    done
 }
