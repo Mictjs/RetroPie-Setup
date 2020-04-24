da526541a1cc14e42e344753e97cff18da281c27
Mike P <63381908+mictjs@users.noreply.github.com>
2020-04-24 19:40:49 -0300

    Update lr-bsnes2014

--- scriptmodules/libretrocores/lr-bsnes2014.sh
+++ scriptmodules/libretrocores/lr-bsnes2014.sh
@@ -9,32 +9,42 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="lr-bsnes"
-rp_module_desc="Super Nintendo emu - bsnes port for libretro"
-rp_module_help="ROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy your SNES roms to $romdir/snes"
+rp_module_id="lr-bsnes2014"
+rp_module_desc="Super Nintendo emu - bsnes (v0.94) port for libretro"
+rp_module_help="ROM Extensions: .smc .sfc .bml .zip .7z\n\nCopy your SNES roms to $romdir/snes"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/bsnes-libretro/libretro/COPYING"
 rp_module_section="opt"
-rp_module_flags="!armv6"
+rp_module_flags="!arm"
 
-function sources_lr-bsnes() {
+function sources_lr-bsnes2014() {
     gitPullOrClone "$md_build" https://github.com/libretro/bsnes-libretro.git
 }
 
-function build_lr-bsnes() {
+function build_lr-bsnes2014() {
     make clean
-    make
-    md_ret_require="$md_build/out/bsnes_accuracy_libretro.so"
+    for i in accuracy balanced performance; do
+        make profile=${i} -j`nproc`
+        md_ret_require="$md_build/out/bsnes_${i}_libretro.so"
+    done
 }
 
-function install_lr-bsnes() {
+function install_lr-bsnes2014() {
     md_ret_files=(
         'out/bsnes_accuracy_libretro.so'
+        'out/bsnes_balanced_libretro.so'
+        'out/bsnes_performance_libretro.so'
         'COPYING'
     )
 }
 
-function configure_lr-bsnes() {
+function configure_lr-bsnes2014() {
     mkRomDir "snes"
+    ensureSystemretroconfig "snes"
+    for j in accuracy balanced performance; do
+        addEmulator 1 "$md_id-${j}" "snes" "$md_inst/bsnes_${j}_libretro.so"
+    done
+    addSystem "snes"
+}
     ensureSystemretroconfig "snes"
 
     addEmulator 1 "$md_id" "snes" "$md_inst/bsnes_accuracy_libretro.so"
