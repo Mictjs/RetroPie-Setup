a6f7391384198bd38e2496c15af7f967356bd4ae
Mike P <63381908+mictjs@users.noreply.github.com>
2020-04-24 23:41:40 -0300

    Update lr-mu - added 7z support

--- scriptmodules/libretrocores/lr-mu.sh
+++ scriptmodules/libretrocores/lr-mu.sh
@@ -11,7 +11,7 @@
 
 rp_module_id="lr-mu"
 rp_module_desc="Palm OS emu - Mu port for libretro"
-rp_module_help="ROM Extensions: .prc .pqa .img .zip\n\nCopy your Palm OS roms to $romdir/palmos\n\nThe Palm OS requires the BIOS files palmos41-en-m515.rom (Palm OS 4.1) copied to $biosdir.\n\nOptional BIOS files:\npalmos40-en-m500.rom (Palm OS 4.0)\npalmos52-en-t3.rom (Palm OS 5.2.1)\npalmos60-en-t3.rom (Palm OS 6.0)\nbootloader-dbvz.rom (MC68VZ328 UART Bootloader)"
+rp_module_help="ROM Extensions: .prc .pqa .img .zip .7z\n\nCopy your Palm OS roms to $romdir/palmos\n\nThe Palm OS requires the BIOS files palmos41-en-m515.rom (Palm OS 4.1) copied to $biosdir.\n\nOptional BIOS files:\npalmos40-en-m500.rom (Palm OS 4.0)\npalmos52-en-t3.rom (Palm OS 5.2.1)\npalmos60-en-t3.rom (Palm OS 6.0)\nbootloader-dbvz.rom (MC68VZ328 UART Bootloader)"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/meepingsnesroms/Mu/master/readme.md"
 rp_module_section="exp"
 rp_module_flags=""
